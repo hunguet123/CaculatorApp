2a07667f65e02e619d224d5a7ecfac22df388933
hunguet123 <hunguet123@gmail.com>
2022-09-28 11:37:28 +0700

    plug in kotlin android extensions

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.GridLayout
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
+
+    fun numberAction(view: View) {
+        if (view is Button) {
+            txt_calculate.append(view.text)
+        }
+    }
+
+    fun operatorAction(view: View) {
+        if (view is Button) {
+            txt_calculate.append(view.text)
+        }
+    }
+
+    fun allClearAction(view: View) {
+        txt_result.text = ""
+        txt_calculate.text = ""
+    }
+
+    fun resultAction(view: View) {}
+
+
 }
