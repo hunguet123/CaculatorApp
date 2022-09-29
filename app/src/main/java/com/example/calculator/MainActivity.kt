cfa9d6eddda10fbc7d7a153c0c78b2a8ca58dfde
hunguet123 <hunguet123@gmail.com>
2022-09-29 23:36:44 +0700

    sap xong con xu ly dau vao

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var calculate = Calculate()
+    private var inputText = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,21 +18,44 @@ class MainActivity : AppCompatActivity() {
     fun numberAction(view: View) {
         if (view is Button) {
             txt_calculate.append(view.text)
+            inputText += view.text.toString()
         }
     }
 
     fun operatorAction(view: View) {
         if (view is Button) {
             txt_calculate.append(view.text)
+            inputText += view.text.toString()
         }
     }
 
     fun allClearAction(view: View) {
         txt_result.text = ""
         txt_calculate.text = ""
+        calculate.clearList()
+        inputText = ""
     }
 
-    fun resultAction(view: View) {}
+    fun resultAction(view: View) {
+        calculate.digitsOperator(inputText)
+        txt_result.text = calculate.result().toString()
+        calculate.clearList()
+    }
+
+    fun percentAction(view: View) {
+        if (view is Button) {
+            txt_calculate.append(view.text)
+            inputText += "÷100"
+        }
+    }
+
+    fun deleteAction(view: View) {
+        if (view is Button) {
+            val textCalculate = txt_calculate.text
+            txt_calculate.text = textCalculate.substring(0, textCalculate.length - 1)
+            inputText = txt_calculate.text.toString()
+        }
+    }
 
 
 }
